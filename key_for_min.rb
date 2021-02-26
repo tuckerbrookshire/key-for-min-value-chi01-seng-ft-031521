4f742ca0a1b5530f2cd3ab80dfd575d28d78517f
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 20:14:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |item, price|
+  name_hash.each do |item, price|
     if #{price} <
     puts item
   end
